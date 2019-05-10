cbeb81a85c4bb3942d95c42a633ce99c8c539365
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2019-05-10 00:23:04 -0700

    add data table plugin in order to manage customer table on client side by rendering data retrieved with api calls

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -12,8 +12,10 @@ namespace Vidly.App_Start
     {
         public MappingProfile()
         {
+            //Domain to Dto
             Mapper.CreateMap<Customer, CustomerDto>();
             Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MembershipType, MembershipTypeDto>();
 
             //Dto to Domain
             Mapper.CreateMap<CustomerDto, Customer>()
