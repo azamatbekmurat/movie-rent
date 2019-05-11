69c464091424a61ecf9c4e1479002edb61a2cbd3
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2019-05-10 19:49:37 -0700

    add data table property to Movies controller as well as add GenreDto class

--- Vidly/Controllers/Api/MoviesController.cs
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -20,10 +21,14 @@ namespace Vidly.Controllers.Api
             _context = new ApplicationDbContext();
         }
 
+        
         //GET api/movies
         public IHttpActionResult GetMovies()
         {
-            var movieDtos = _context.Movies.ToList().Select(Mapper.Map<Movie, MovieDto>);
+            var movieDtos = _context.Movies
+                .Include(m => m.Genre)
+                .ToList()
+                .Select(Mapper.Map<Movie, MovieDto>);
 
             return Ok(movieDtos);
 
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -26,9 +26,8 @@ namespace Vidly.Controllers
         // GET: Movies/Random
         public ViewResult Index()
         {
-            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
-            return View(movies);
+            return View();
             
         }
 
