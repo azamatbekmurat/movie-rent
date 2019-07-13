6a7d2f3234236c07add7571bcfb150d93db9eff5
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2019-07-12 22:40:28 -0700

    add colors and add button to customer page to add new customers

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -28,6 +28,7 @@ namespace Vidly
             
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-theme.css",
                       "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/typeahead.css",
                       "~/Content/toastr.css",
